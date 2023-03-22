06a3c7fe171f5c8df6d0d375620c69dbd31eb371
Denis Kiselev <kiselevden911@yandex.ru>
2023-03-22 21:11:00 +0500

    web hook refactor

--- src/Dto/WebHookList.php
+++ src/Dto/WebHookList.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 namespace CdekSDK2\Dto;
 
-use CdekSDK2\BaseTypes\WebHook;
 use JMS\Serializer\Annotation\Type;
 
 class WebHookList implements IList
 {
     /**
      * Список ПВЗ
-     * @Type("array<CdekSDK2\BaseTypes\WebHook>")
-     * @var WebHook[]
+     * @Type("array<CdekSDK2\Dto\WebHookDto>")
+     * @var WebHookDto[]
      */
     public $items = [];
 }
